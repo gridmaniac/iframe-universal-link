d3c1fc481637ae3446835605cbd2529b6e91f371
Anton Pushkarev <a.pushkarev@kassir.ru>
2024-11-12 14:14:26 +0400

    first commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <a
+      href="bank100000000111://qr.nspk.ru/AD20005APGJ17D4H8T7ORJIIUBVJF35Q?type=02&bank=100000000004&sum=210000&cur=RUB&crc=C50C"
+      >App Link</a
+    >
+  </div>
+</template>
