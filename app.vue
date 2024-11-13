e27e30ca9b4705def47b3a1e8d36cd735c3d7e4c
Anton Pushkarev <a.pushkarev@kassir.ru>
2024-11-13 20:33:25 +0400

    chore iframe mode

--- app.vue
+++ app.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <a
-      target="_top"
-      href="bank100000000111://qr.nspk.ru/AD20005APGJ17D4H8T7ORJIIUBVJF35Q?type=02&bank=100000000004&sum=210000&cur=RUB&crc=C50C"
-      >App Link</a
-    >
+    <iframe src="https://universal-links.netlify.app/" />
   </div>
 </template>
